00de5d2320db67de702d850730af9dbf5ba5e3c3
gawon-33 <kaon6543@gmail.com>
2026-07-07 09:36:23 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ slider.addEventListener('wheel',(e)=>{
     e.preventDefault();
     slider.scrollLeft += e.deltaY;
 });
+
+// EXPLORE AGAIN 버튼 -> 첫 화면(맨 위)으로 부드럽게 이동
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
